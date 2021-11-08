b6996669d7d3e9f7735065bb1df8c301bba2ac20
Clement <clement.casamayou@gmail.com>
2021-11-08 11:06:54 +0100

    chore(develop): Setup project

---
+++ app/src/main/java/com/example/dummyapp/ui/modules/userlist/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.dummyapp.ui.modules.userlist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.dummyapp.R
+
+class MainActivity: AppCompatActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
